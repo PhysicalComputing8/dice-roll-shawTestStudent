b62d16fa314964c8e4a69c6a89743437d9d31f24
shawTestStudent <71569341+shawTestStudent@users.noreply.github.com>
2020-09-21 20:37:25 -0400

    shortened intro message. fixed numerical range to reflect actual dice.

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 input.onGesture(Gesture.Shake, function () {
-	
+    // A variable is a place where your program can store a value. We need a place to store the result of the roll, so I created a variable called roll.
+    //  I have the program generate a random number between 1 and 6, and I assign that value to the roll variable
+    roll = randint(1, 6)
+    // Why is this code grayed out?  It isn't connected to anything that will trigger it to execute. In order to execute, a block has to be inside of an event such as "on start" or "on Shake" or "forever".
+    basic.showNumber(roll)
 })
 // Code added here will run once when the program starts
-basic.showString("Shake to roll the dice")
+let roll = 0
+basic.showString("Shake to roll")
