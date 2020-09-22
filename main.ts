1d476422fdb9d3d081aaf5d178db0c8479d7cdc1
shawTestStudent <71569341+shawTestStudent@users.noreply.github.com>
2020-09-21 21:20:25 -0400

    added visual dice display of pips

--- main.ts
+++ main.ts
@@ -3,8 +3,55 @@ input.onGesture(Gesture.Shake, function () {
     // A variable is a place where your program can store a value. We need a place to store the result of the roll, so I created a variable called roll.
     //  I have the program generate a random number between 1 and 6, and I assign that value to the roll variable
     roll = randint(1, 6)
-    // Why is this code grayed out?  It isn't connected to anything that will trigger it to execute. In order to execute, a block has to be inside of an event such as "on start" or "on Shake" or "forever".
-    basic.showNumber(roll)
+    if (roll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (roll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (roll == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+    } else if (roll == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (roll == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
 })
 // Code added here will run once when the program starts
 let roll = 0
