2b66537d751a63578b156accb4946b8cc094750a
Trevor Shaw <shawt@genlrn.com>
2020-09-21 20:51:05 -0400

    Teacher Feedback

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// Great job. You moved the commands inside of the onShake event trigger. This way, the code will get executed when the micro:bit is shaken.
 input.onGesture(Gesture.Shake, function () {
     // A variable is a place where your program can store a value. We need a place to store the result of the roll, so I created a variable called roll.
     //  I have the program generate a random number between 1 and 6, and I assign that value to the roll variable
