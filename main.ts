3a7277447856903ec57e9709f36b81e22211fbf4
Trevor Shaw <shawt@genlrn.com>
2020-09-26 09:44:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ input.onGesture(Gesture.Shake, function () {
         basic.showLeds(`
             # . . . #
             . # . # .
-            . . # . .
+            # # # # #
             . # . # .
             # . . . #
             `)
