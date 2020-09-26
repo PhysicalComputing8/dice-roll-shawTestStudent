198b485c404cd36f5757eb38e8dc0c2d2d152912
shawTestStudent <71569341+shawTestStudent@users.noreply.github.com>
2020-09-26 09:41:45 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 // Great job. You moved the commands inside of the onShake event trigger. This way, the code will get executed when the micro:bit is shaken.
 input.onGesture(Gesture.Shake, function () {
     // A variable is a place where your program can store a value. We need a place to store the result of the roll, so I created a variable called roll.
-    //  I have the program generate a random number between 1 and 6, and I assign that value to the roll variable
+    // I have the program generate a random number between 1 and 6, and I assign that value to the roll variable
     roll = randint(1, 6)
     if (roll == 1) {
         basic.showLeds(`
-            # . . . .
-            . # . . .
+            # . . . #
+            . # . # .
             . . # . .
-            . . . # .
-            . . . . #
+            . # . # .
+            # . . . #
             `)
     } else if (roll == 2) {
         basic.showLeds(`
@@ -53,6 +53,5 @@ input.onGesture(Gesture.Shake, function () {
             `)
     }
 })
-// Code added here will run once when the program starts
 let roll = 0
 basic.showString("Shake to roll")
